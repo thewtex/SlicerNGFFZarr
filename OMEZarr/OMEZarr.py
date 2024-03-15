aa536ede8fa5cdf9ec119d5183c03ee122bdc3f3
Matt McCormick <matt.mccormick@kitware.com>
2024-03-15 17:13:53 -0400

    DOC: More module init metadata

--- OMEZarr/OMEZarr.py
+++ OMEZarr/OMEZarr.py
@@ -29,21 +29,19 @@ class OMEZarr(ScriptedLoadableModule):
 
     def __init__(self, parent):
         ScriptedLoadableModule.__init__(self, parent)
-        self.parent.title = _("OMEZarr")  # TODO: make this more human readable by adding spaces
-        # TODO: set categories (folders where the module shows up in the module selector)
-        self.parent.categories = [translate("qSlicerAbstractCoreModule", "Examples")]
-        self.parent.dependencies = []  # TODO: add here list of module names that this module requires
-        self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
+        self.parent.title = _("OME-Zarr")
+        self.parent.categories = [translate("qSlicerAbstractCoreModule", "Informatics"), translate("qSlicerAbstractCoreModule", "Converters")]
+        self.parent.dependencies = []
+        self.parent.contributors = ["Matt McCormick (Kitware, Inc.)"]
         # TODO: update with short description of the module and a link to online module documentation
         # _() function marks text as translatable to other languages
         self.parent.helpText = _("""
-This is an example of scripted loadable module bundled in an extension.
+Load and save OME-Zarr images.
 See more information in <a href="https://github.com/organization/projectname#OMEZarr">module documentation</a>.
 """)
         # TODO: replace with organization, grant and thanks
         self.parent.acknowledgementText = _("""
-This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
-and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+This file was originally developed by Matt McCormick, Kitware Inc.
 """)
 
         # Additional initialization step after application startup is complete
